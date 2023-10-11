812a505ca9b5e2b5cca0e73222e89d70169ded8c
Игорь Сечинов <sechinov.i@ms.beltelecom.by>
2023-10-11 15:44:44 +0300

    upload test

--- src/js/index.js
+++ src/js/index.js
@@ -41,7 +41,7 @@ function closeOnClick() {
 $(function () {
 	$(document).scroll(function () {
 		var $header = $("header");
-		var $header__container = $(".navigation");
+		var $header__container = $(".wrapper-nav");
 		$header.toggleClass('scrolled', $(this).scrollTop() > $header.height());
 		$header__container.toggleClass('scrolled-border', $(this).scrollTop() > $header.height());
 	});
